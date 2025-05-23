ee0da3cf3f2f5a52572b6430e408015662cce91a
Alex Savelev <savaqwerty2@gmail.com>
2025-05-23 18:48:12 +0200

    update hw

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,7 +6,7 @@ import MovieModal from "../MovieModal/MovieModal";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import { fetchMovies } from "../../services/movieService";
-import type { Movie } from "../../types/movies";
+import type { Movie } from "../../types/movie";
 import css from "./App.module.css";
 
 
--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { type Movie } from "../../types/movies";
+import { type Movie } from "../../types/movie";
 import css from "./MovieModal.module.css"
 
 interface MovieModalProps {
